b79cff546cbcd5567ef772dda2ace3f50e4e5d20
syedabuthaheer <syedpcy@gmail.com>
2026-02-08 22:46:10 +0530

    Update Baseoneprt.java

--- src/test/java/com/Finaltest/Baseoneprt.java
+++ src/test/java/com/Finaltest/Baseoneprt.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -21,11 +20,12 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.testng.annotations.Optional;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Baseoneprt {
 	public static ThreadLocal<WebDriver> tdriver = new ThreadLocal<>();
